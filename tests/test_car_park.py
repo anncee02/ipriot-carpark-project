e2db0e791bb8560986e9f611b35bfaf80c12b734
20133854@tafe.wa.edu.au <20133854@tafe.wa.edu.au>
2024-11-25 19:20:08 +1100

    Added unit tests for logging activity

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -5,6 +5,7 @@ from pathlib import Path
 class TestCarPark(unittest.TestCase):
       def setUp(self):
          self.car_park = CarPark("123 Example Street", 100)
+          self.test_log_file = Path("test_log.txt") # Defines the log file here
 
       def test_car_park_initialized_with_all_attributes(self):
          self.assertIsInstance(self.car_park, CarPark)
@@ -14,6 +15,7 @@ class TestCarPark(unittest.TestCase):
          self.assertEqual(self.car_park.sensors, [])
          self.assertEqual(self.car_park.displays, [])
          self.assertEqual(self.car_park.available_bays, 100)
+         self.assertEqual(self.car_park.log_file, Path("log.txt"))
 
       def test_add_car(self):
          self.car_park.add_car("TEST-001")
@@ -42,7 +44,15 @@ class TestCarPark(unittest.TestCase):
       def test_register_raises_type_error(self):
          with self.assertRaises(TypeError):
             self.car_park.register("Not a Sensor or Display")
-    def test_car_logged_when_entering(self):
+
+      def test_log_file_created(self):
+          new_carpark = CarPark("123 Example Street", 100, log_file=self.test_log_file)
+          self.assertTrue(Path(self.test_log_file).exists())
+
+      def tearDown(self):
+          Path("new_log.txt").unlink(missing_ok=True)
+
+      def test_car_logged_when_entering(self):
         new_carpark = CarPark("123 Example Street", 100,
                               log_file="new_log.txt")  # TODO: change this to use a class attribute or new instance variable
         self.car_park.add_car("NEW-001")
@@ -63,6 +73,7 @@ class TestCarPark(unittest.TestCase):
         self.assertIn(last_line, "NEW-001")  # check plate entered
         self.assertIn(last_line, "exited")  # check description
         self.assertIn(last_line, "\n")  # check entry has a new line
+
 
 
 if __name__ == "__main__":
