1ddc7f6b5d4badf4aa8f061eb580eb2954728ba1
20133854@tafe.wa.edu.au <20133854@tafe.wa.edu.au>
2024-11-25 20:58:45 +1100

    WIP: Commit changes before merging

--- src/car_park.py
+++ src/car_park.py
@@ -1,5 +1,8 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime # we'll use this to timestamp entries
+
 class CarPark:
     """ Holds the state and behaviours of a car park"""
 
@@ -8,13 +11,17 @@ class CarPark:
                  capacity,
                  plates = None,
                  sensors = None,
-                 displays = None
+                 displays = None,
+                 log_file=Path("log.txt")
                  ):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # creates the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
     @property
     def available_bays(self):
@@ -52,3 +59,17 @@ class CarPark:
                             "Temperature": 42, }
                             )
             print(f"Updating: {display}")
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+        self._log_car_activity(plate, "entered")
+
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+        self.update_displays()
+        self._log_car_activity(plate, "exited")
